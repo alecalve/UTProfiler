d84db856996f0f7fb4246a88db28fde683d94d6e
alecalve <antoine@alc.io>
2014-06-05 12:01:32 +0200

    Passage d’un QLineInput à une QComboBox pour les notes d’UV

--- qt/addsemestredialog.cpp
+++ qt/addsemestredialog.cpp
@@ -6,16 +6,24 @@
 #include "src/exceptions.hpp"
 
 #define UVM UvManager::getInstance()
+#define NUM NoteUVManager::getInstance()
 
 AddSemestreDialog::AddSemestreDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AddSemestreDialog) {
     ui->setupUi(this);
+    QStringList notes;
+    std::vector<NoteUV> iterator = NUM->iterator();
+    for(auto it=iterator.begin(); it!=iterator.end(); it++) {
+        notes<<it->nom;
+    }
+    ui->noteBox->addItems(notes);
+
 }
 
 void AddSemestreDialog::uvAdded() {
     QString code = ui->codeEdit->text();
-    QString note = ui->noteEdit->text();
+    QString note = ui->noteBox->currentText();
 
     try {
         UVM->getItem(code);
