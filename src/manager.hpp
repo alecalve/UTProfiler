543d59fef8fadda0f99ea02b99acd471f2a8a871
alecalve <antoine@alc.io>
2014-06-12 21:43:34 +0200

    Passage de la gestion des sauvegardes des managers à MainWindow

--- src/manager.hpp
+++ src/manager.hpp
@@ -3,8 +3,10 @@
 
 #include <vector>
 #include <string>
+#include <iostream>
 
-#include "abstractio.hpp"
+#include <QStringList>
+
 #include "singleton.hpp"
 #include "exceptions.hpp"
 
@@ -36,11 +38,6 @@ template<class T> class Manager {
         elements.push_back(t);
     }
 
-    //! Indique si une stratégie de sauvegarde a été définie
-    bool hasPolicyDefined() {
-        return (ioPolicy != 0);
-    }
-
     //! Retourne un objet identifié par une string (QString)
     T& getItem(const QString &s) const {
         int index = -1;
@@ -55,20 +52,18 @@ template<class T> class Manager {
         throw Exception("Objet non trouvé.");
     }
 
-    //! Charge les UVs présentes dans la sauvegarde définie par la stratégie ioPolicy
-    inline void load() {
-        ioPolicy->load();
-    }
-
-    //! Sauvegarde les objets sauvegardés en utilisant la stratégie définie auparavant
-    void save() { ioPolicy->save(); }
 
     //! Retourne un itérateur (vector) contenant les objets du manager
     std::vector<T> iterator() { return elements; }
 
-    //! Détermine la stratégie de sauvegarde et de chargement des objets
-    void setPolicy(AbstractIo *p) {
-        ioPolicy = p;
+
+    //! Retourne un liste des noms des objets du manager
+    QStringList getItemNameList() {
+        QStringList list;
+        for(auto it=elements.begin(); it!=elements.end(); it++) {
+            list<<it->getName();
+        }
+        return list;
     }
 
     //! Suppression d'un élément.
@@ -77,7 +72,6 @@ template<class T> class Manager {
         bool found = false;
 
         for(auto it=elements.begin(); !found && it!=elements.end(); it++) {
-            if (found) { break; }
             if (*it == item) {
                 //On met it = devant pour ne pas invalider l’iterateur
                 //Sinon si on supprime des élements pendant une itération
@@ -88,20 +82,13 @@ template<class T> class Manager {
         }
     }
 
-    ~Manager() { delete ioPolicy; }
-
   private:
-    Manager() {
-        ioPolicy = 0;
-    }
+    Manager() {}
     Manager(const Manager &) {}
     Manager operator=(const Manager&) {}
 
     //! Stocke les éléments gérés par le manager
     std::vector<T> elements;
-
-    //! Défini le comportement de sauvegarde des objets
-    AbstractIo *ioPolicy;
 
 };
 
