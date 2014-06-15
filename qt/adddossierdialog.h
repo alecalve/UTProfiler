f081758578b0fcd2050c08a9cc32fb8d6a78656a
alecalve <antoine@alc.io>
2014-06-15 17:27:15 +0200

    Commentaires et gestion des dossiers

--- qt/adddossierdialog.h
+++ qt/adddossierdialog.h
@@ -16,17 +16,32 @@ class AddDossierDialog : public QDialog
 public:
     explicit AddDossierDialog(QWidget *parent = 0);
     ~AddDossierDialog();
+
+    //! En cas d’édition, défini le dossier à éditer
     void setDossier(const QString& name);
 
+    //! En cas de création, crée le dossier vierge
+    void setDossier();
+
 public slots:
+
+    //! Slot activé en cas d’ajout de formation, met à jour l’UI et ajoute la
+    //! formation à la QStringList formations
     void formationAdded();
+
+    //! Slot activé à la fermeture du dialog, crée/update le dossier
     void createDossier();
+
+    //! Slot activé à l’ajout d’un semestre, met à jour l’UI
+    void semestreAdded();
 
 private:
     Ui::AddDossierDialog *ui;
-    QStringList formations;
 
+    //! Indique si le dialog doit éditer ou créer un dossier
     bool editionMode;
+
+    //! Dossier en cours de création ou d’édition
     Dossier dossier;
 };
 
